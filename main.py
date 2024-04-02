bee1e42b469bb48dd5f1640912f29998f3508f04
felop <contact@felixlop.com>
2024-04-02 14:07:05 +0200

    10->7

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ a, b = 1, 1
 f = lambda x:a*x+b
 
 for i in tqdm(range(iter)):
-    M_satisf[i] = np.mean([sim.run(crousEff=f(i)*2, crousPrice=eff2price(f(i)), showResults=False) for _ in range(10)])
+    M_satisf[i] = np.mean([sim.run(crousEff=f(i)*2, crousPrice=eff2price(f(i)), showResults=False) for _ in range(7)])
 
 print(M_satisf)
 X = f(np.arange(0, iter, 1))
